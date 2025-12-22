fc9291b77ee1d766716a4bd8b7bbc613f9b54ffc
JManion32 <jmanion32@gmail.com>
2025-12-22 17:04:55 -0500

    Moved site title higher

--- site/pages/Home.tsx
+++ site/pages/Home.tsx
@@ -5,36 +5,38 @@ function App() {
   return (
     <>
         <div className="home-page-container">
-            <h1 className="site-title"><span className="site-title-1">Super</span> <span className="site-title-2">Strikers</span></h1>
-            <p
-                className="name-warning"
-            >
-                Please choose a clean nickname!
-            </p>
-            <input
-                type="text"
-                maxLength={24}
-                className="name-input"
-                placeholder="Enter Nickname"
-                title="Enter your nickname"
-                /*
-                value={nickname}
-                onChange={(e) => setNickname(e.target.value)}
-                onKeyDown={(e) => {
-                    if (e.key === "Enter" && nickname) {
-                        e.preventDefault();
-                        startGame();
-                    }
-                }}
-                */
-            />
-            <div className="start-btns-container">
-                <button className="start-btn gradient-border">
-                    <span className="gradient-text">Find Match</span>
-                </button>
-                <button className="start-btn gradient-border">
-                    <span className="gradient-text">Custom Match</span>
-                </button>
+            <div className="home-content-container">
+                <h1 className="site-title"><span className="site-title-1">Super</span> <span className="site-title-2">Strikers</span></h1>
+                <p
+                    className="name-warning"
+                >
+                    Please choose a clean nickname!
+                </p>
+                <input
+                    type="text"
+                    maxLength={24}
+                    className="name-input"
+                    placeholder="Enter Nickname"
+                    title="Enter your nickname"
+                    /*
+                    value={nickname}
+                    onChange={(e) => setNickname(e.target.value)}
+                    onKeyDown={(e) => {
+                        if (e.key === "Enter" && nickname) {
+                            e.preventDefault();
+                            startGame();
+                        }
+                    }}
+                    */
+                />
+                <div className="start-btns-container">
+                    <button className="start-btn gradient-border">
+                        <span className="gradient-text">Find Match</span>
+                    </button>
+                    <button className="start-btn gradient-border">
+                        <span className="gradient-text">Custom Match</span>
+                    </button>
+                </div>
             </div>
         </div>
         <div className="home-footer-container">
