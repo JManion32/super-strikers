1eef8c14b52e90593d8c299555aba05dedb57f2b
JManion32 <jmanion32@gmail.com>
2025-12-25 17:23:37 -0500

    Ran ESlint

--- site/eslint.config.ts
+++ site/eslint.config.ts
@@ -2,10 +2,34 @@ import js from "@eslint/js";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
-import { defineConfig } from "eslint/config";
+import { defineConfig, globalIgnores } from "eslint/config";
 
 export default defineConfig([
   { files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
   tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
+  globalIgnores([
+    // dependencies
+    "**/node_modules/**",
+
+    // build output (future-proof)
+    "**/dist/**",
+    "**/build/**",
+
+    // config & tooling files
+    ".config/**",
+    "*.config.js",
+    "*.config.ts",
+    "*.config.mjs",
+    ".prettierrc*",
+    ".stylelint*",
+
+    // TypeScript configs
+    "tsconfig*.json",
+
+    // misc
+    "package-lock.json",
+    "README.md",
+    "index.html"
+  ]),
 ]);
